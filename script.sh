0688a6ff3538426500e0d0f91bbfd44275ce7b88
duoxu <duoxu@microsoft.com>
2016-09-07 15:03:29 -0700

    make script paramterable

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.2.0-25/omsagent-1.2.0-25.universal.x64.sh -O /tmp/omsagent.x64.sh
-sudo sh /tmp/omsagent.x64.sh --upgrade -w c35e28bd-b2cb-4413-acb2-cf2da22186b6 -s qBVv4pXXcVgY+9uX3JEVV1b2MsFNFnlFKCZzxwGw0meTebCNyMYqWM8dXSHYj/WODQPIN4mM490BCrsG88sLZA==
+sudo sh /tmp/omsagent.x64.sh --upgrade -w $1 -s $2
 
 
 sudo wget https://raw.githubusercontent.com/duoxu/darfo/master/hbase.workernode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/hbase.workernode.conf
