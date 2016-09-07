b6ed8dd77234ed6f0a4443ebc413d01a6447fb27
duoxu <duoxu@microsoft.com>
2016-09-07 15:42:13 -0700

    update script

--- script.sh
+++ script.sh
@@ -3,8 +3,13 @@
 wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.2.0-25/omsagent-1.2.0-25.universal.x64.sh -O /tmp/omsagent.x64.sh
 sudo sh /tmp/omsagent.x64.sh --upgrade -w $1 -s $2
 
-
-sudo wget https://raw.githubusercontent.com/duoxu/darfo/master/hbase.workernode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/hbase.workernode.conf
+if [[ $HOSTNAME == wn* ]];
+then
+  sudo wget https://raw.githubusercontent.com/duoxu/darfo/master/hbase.workernode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/hbase.workernode.conf
+elif [[ $HOSTNAME == zk* ]];
+then
+  sudo wget https://raw.githubusercontent.com/duoxu/darfo/master/hbase.zookeeper.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/hbase.zookeeper.conf
+fi
 
 sudo wget https://raw.githubusercontent.com/duoxu/darfo/master/filter_hdinsight.rb -O /opt/microsoft/omsagent/plugin/filter_hdinsight.rb
 
