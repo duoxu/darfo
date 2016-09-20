89343bb4dbac0a36650f0bfd7f274bce7a54f799
duoxu <duoxu@microsoft.com>
2016-09-20 10:58:41 -0700

    add simple install file

---
+++ script2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.2.0-25/omsagent-1.2.0-25.universal.x64.sh -O /tmp/omsagent.x64.sh
+sudo sh /tmp/omsagent.x64.sh --upgrade -w $1 -s $2
+
+sudo service omsagent restart
